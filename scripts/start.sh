a6a96e16f346abb595f2d06e20036cfe46263545
Dariusz Bursztynowski <dariusz.bursztynowski@orange.com>
2019-06-22 01:04:30 +0200

    Update start.sh

--- scripts/start.sh
+++ scripts/start.sh
@@ -4,9 +4,9 @@ set -e
 
 port=$(ctx node properties port)
 
-ctx logger info "Starting HTTP server on port ${port}"
+ctx logger info "Starting HTTP server on port ${port}."
 
-ctx logger info "Waiting for server to launch on port ${port}"
+ctx logger info "Waiting for server to launch on port ${port}."
 url="http://localhost:${port}"
 
 server_is_up() {
@@ -19,7 +19,7 @@ server_is_up() {
 			return 0
 		fi
 	else
-		ctx logger error "Both curl, wget not found in path"
+		ctx logger error "Both curl, wget not found in path."
 		exit 1
 	fi
 	return 1
@@ -29,7 +29,7 @@ STARTED=false
 for i in $(seq 1 15)
 do
 	if server_is_up; then
-		ctx logger info "Server is up on port ${port}"
+		ctx logger info "Server is up on port ${port}."
 		STARTED=true
     	break
 	else
